751124b49de5db1ad70eeb23de1534c097d1d415
victor arcanjo <vtarcanjo@hotmail.com>
2024-01-09 08:12:37 -0300

    feat: add gradient text

---
+++ src/components/GradientText/index.tsx
@@ -0,0 +1,15 @@
+import { GradientH1 } from './styles';
+
+interface GradientTextPros {
+  text: string;
+  classes: string;
+}
+
+export const GradientText = ({ text, classes }: GradientTextPros) => {
+  return (
+    <GradientH1 className={ classes }>
+      {text}
+    </GradientH1>
+  )
+}
+
