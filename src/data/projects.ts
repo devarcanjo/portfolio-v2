f38e510126c548333f705c406aecd2ebbcb6e5e0
victor arcanjo <vtarcanjo@hotmail.com>
2024-01-13 13:16:22 -0300

    feat: add data projects

---
+++ src/data/projects.ts
@@ -0,0 +1,11 @@
+const projects = [
+  {
+    image: "",
+    technologies: ["ReactJs", "React Hooks", "Styled components", "RTL"],
+    githubLink: "https://github.com/devarcanjo/portfolio-v2",
+    deployLink: "",
+    description: `Meu portfolio, onde falo um pouco sobre eu mesmo e as tecnologias que conheço (esse site que você está utilizando)`,
+  },
+];
+
+export default projects;
